6a67c2f73ecd48650e7c86e8afa706ad41921020
Devin Lafolette <devinlafolette@gmail.com>
2024-02-04 18:22:39 -0600

    Followed the video EXACTLY yet still cannot get it to work

---
+++ Assets/Scripts/TouchManagerScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManagerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private PlayerInput playerInput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+        touchPositionAction = playerInput.actions.FindAction("TouchPosition");
+        //can also be accomplished with touchPressAction = playerInput.actions["TouchPress"];
+    }
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+    }
+}
