a0ecd191ffbd7176ac629d65d4504d19fd4f6c6a
Devin Lafolette <devinlafolette@gmail.com>
2024-02-18 16:11:30 -0600

    Made a working StateMachine but need to fix onClick

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,52 +2,150 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public static Button swordBtn;
-    public static Button sniperBtn;
-    public static Button spellBtn;
-
-    //public bool buttonPressed;
-
-    private GamePlayState playState;
-
-    private GameFSM _stateMachine;
-
-    public int combatMode;
-
-    public GameController (GameFSM stateMachine)
+    public enum State
     {
-        _stateMachine = stateMachine;
+        Play, Combat, Dodge, Win, Lose
     }
+
+    public State CurrentState = State.Play;
+
+    [SerializeField] private PartyManager partyManager;
+    [SerializeField] private EnemyManager enemyManager;
+
+    [SerializeField] public Button swordBtn;
+    [SerializeField] public Button continueBtn;
+
+    [SerializeField] public GameObject playerTurnUI;
+    [SerializeField] public GameObject winUI;
+    [SerializeField] public GameObject loseUI;
+
+    [SerializeField] public TextMeshProUGUI stateText;
+
+    private bool buttonIsPressed = false;
+    private int timer;
 
     private void Awake()
     {
+        CurrentState = State.Play;
         swordBtn = GameObject.Find("Sword_btn").GetComponent<Button>();
-        //sniperBtn = GameObject.Find("Sniper_btn").GetComponent<Button>();
-        //spellBtn = GameObject.Find("Spell_btn").GetComponent<Button>();
+        //continueBtn = GameObject.Find("Continue_btn").GetComponent<Button>();
+        playerTurnUI = GameObject.Find("PlayerTurn_pnl");
+        winUI = GameObject.Find("Win_pnl");
+        loseUI = GameObject.Find("Lose_pnl");
+        stateText = GameObject.Find("StateChangingText").GetComponent<TextMeshProUGUI>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        swordBtn.onClick.AddListener(Sword);
-        //sniperBtn.onClick.AddListener(playState.Sniper);
-        //spellBtn.onClick.AddListener(playState.Spell);
+        switch(CurrentState)
+        {
+            case State.Play:
+                DoPlayState();
+                break;
+            case State.Combat:
+                DoCombatState();
+                break;
+            case State.Dodge:
+                DoDodgeState();
+                break;
+            case State.Win:
+                DoWinState();
+                break;
+            case State.Lose:
+                DoLoseState();
+                break;
+        }
     }
-    public void Sword()
+
+    private void DoPlayState()
     {
-        combatMode = 1;
-        _stateMachine.ChangeState(_stateMachine.CombatState);
+        //Debug.Log("Play");
+        stateText.text = "Play";
+        swordBtn.onClick.AddListener(buttonPressed);
+        if(buttonIsPressed == true)
+        {
+            playerTurnUI.SetActive(false);
+            ChangeState(State.Combat);
+        }
+        else
+        {
+            buttonIsPressed = false;
+            playerTurnUI.SetActive(true);
+        }
     }
-    public void Sniper()
+
+    private void DoCombatState()
     {
-        combatMode = 2;
-        _stateMachine.ChangeState(_stateMachine.CombatState);
+        //Debug.Log("Combat");
+        stateText.text = "Combat";
+        timer++;
+        if (enemyManager.enemyHealth > 0 && timer >= 300)
+        {
+            ChangeState(State.Dodge);
+        }
+        else if (enemyManager.enemyHealth <= 0 && timer >= 300)
+        {
+            ChangeState(State.Win);
+        }
     }
-    public void Spell()
+
+    private void DoDodgeState()
     {
-        combatMode = 3;
-        _stateMachine.ChangeState(_stateMachine.CombatState);
+        //Debug.Log("Dodge");
+        stateText.text = "Dodge";
+        timer++;
+        if (partyManager.partyHealth > 0 && timer >= 300)
+        {
+            ChangeState(State.Play);
+        }
+        else if (partyManager.partyHealth <= 0)
+        {
+            ChangeState(State.Lose);
+        }
+    }
+
+    private void DoWinState()
+    {
+        //Debug.Log("Win");
+        stateText.text = "Win";
+        continueBtn.onClick.AddListener(buttonPressed);
+        if(buttonIsPressed == true)
+        {
+            ChangeState(State.Play);
+        }
+        else
+        {
+            buttonIsPressed = false;
+        }
+    }
+
+    private void DoLoseState()
+    {
+        //Debug.Log("Lose");
+        stateText.text = "Lose";
+    }
+
+    public void ChangeState(State newState)
+    {
+        if (CurrentState == newState)
+        {
+            return;
+        }
+        timer = 0;
+        CurrentState = newState;
+    }
+
+    public void buttonPressed()
+    {
+        buttonIsPressed = true;
+    }
+
+    public void buttonNotPressed()
+    {
+        buttonIsPressed = false;
     }
 }
